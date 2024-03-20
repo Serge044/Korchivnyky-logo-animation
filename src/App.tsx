63c626839aef5fddc349858b9497bec1dc2a87de
Serge044 <serhiima@wix.com>
2024-03-20 11:54:29 +0200

    improved animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,83 @@
-import './App.css';
+import { useRef } from 'react';
+
+import styles from './App.module.css';
 import wheelAndWrenches from './images/wheel-and-wrenches2.svg';
 import wheel from './images/wheel2.svg';
 import title from './images/title2.svg';
-import yellewBlue from './images/yellow-blue.svg';
+import yellowBlue from './images/yellow-blue.svg';
 import wheelFadeInOut from './images/wheel-fadeInOut2.svg';
 import darkCircle from './images/dark-circle.svg';
 
-const App = () => {
+const AnimatedLogo = () => {
+  const groupRef = useRef<SVGGElement>(null);
+
   const handleGroupClick = () => {
-    const group = document.getElementById('group');
+    const group = groupRef.current;
     if (group) {
-      group.classList.add('active');
+      group.classList.add(styles.active);
 
       setTimeout(() => {
-        group.classList.remove('active');
+        group.classList.remove(styles.active);
       }, 1500);
     }
   };
+
   return (
-    <div id='container'>
-      <div>
-        <svg
-          id='mainCanvas'
-          width='450'
-          height='450'
-          onClick={handleGroupClick}
-        >
-          <g id='group'>
-            <image
-              id='yellowBlue'
-              href={yellewBlue}
-              height='350'
-              width='350'
-              x='50'
-              y='50'
-            />
-            <image
-              id='wheelAndWrenches'
-              href={wheelAndWrenches}
-              height='350'
-              width='350'
-              x='50'
-              y='50'
-            />
-            <image
-              id='darkCircle'
-              href={darkCircle}
-              height='350'
-              width='350'
-              x='50'
-              y='50'
-            />
-            <image
-              id='wheel'
-              href={wheel}
-              height='350'
-              width='350'
-              x='50'
-              y='50'
-            />
-            <image
-              id='wheelFadeInOut'
-              href={wheelFadeInOut}
-              height='350'
-              width='350'
-              x='50'
-              y='50'
-            />
-            <image
-              id='title'
-              href={title}
-              height='350'
-              width='350'
-              x='50'
-              y='50'
-            />
-          </g>
-        </svg>
-      </div>
+    <div>
+      <svg className={styles.mainCanvas} width='200' height='200'>
+        <g className={styles.group} ref={groupRef} onClick={handleGroupClick}>
+          <image
+            className={styles.yellowBlue}
+            href={yellowBlue}
+            height='100'
+            width='100'
+            x='50'
+            y='50'
+          />
+          <image
+            className={styles.wheelAndWrenches}
+            href={wheelAndWrenches}
+            height='100'
+            width='100'
+            x='50'
+            y='50'
+          />
+          <image
+            className={styles.darkCircle}
+            href={darkCircle}
+            height='100'
+            width='100'
+            x='50'
+            y='50'
+          />
+          <image
+            className={styles.wheel}
+            href={wheel}
+            height='100'
+            width='100'
+            x='50'
+            y='50'
+          />
+          <image
+            className={styles.wheelFadeInOut}
+            href={wheelFadeInOut}
+            height='100'
+            width='100'
+            x='50'
+            y='50'
+          />
+          <image
+            className={styles.title}
+            href={title}
+            height='100'
+            width='100'
+            x='50'
+            y='50'
+          />
+        </g>
+      </svg>
     </div>
   );
 };
 
-export default App;
+export default AnimatedLogo;
